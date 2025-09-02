eb1f038c14e1ed20c24ebebd85cdb8a757d62c22
Tirthazoldeo <zoldeo.tirtha@webseeder.tech>
2025-09-02 11:52:29 +0530

    Update new.py

--- new.py
+++ new.py
@@ -41,5 +41,27 @@ for document in documents:
     wordPatterns=[lemmatizer.lemmatize(word.lower()) for word in wordPatterns]
     for word in words: bag.append(1) if word in wordPatterns else bag.append(0)
     
+    outputRow=list(outputEmpty)
+    outputRow[classes.index(document[1])]=1
+    training.append(bag+outputRow)
 
+random.shuffle(training)
+training=np.array(training)
+
+trainX=training[:, :len(words)]
+trainY=training[:, :len(words):]
+
+model=tf.keras.Sequential()
+
+model.add(tf.keras.layers.Dense(128,input_shape=(len(trainX[0]),),activation='relu'))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(64,activation='relu'))
+model.add(tf.keras.layers.Dense(len(trainY[0]),activation="softmax"))
+
+sgd = tf.keras.optimizers.SGD(learning_rate=0.001,momentum=0.9,nesterov=True)
+
+model.compile(loss='categorical_crossentropy',optimizer=sgd, metrics= ['accuracy'])
+hist= model.fit(np.array(trainX),np.array(trainY),epochs=200,batch_size=5,verbose=1)
+model.save("chatbot_webseedermodel.keras")
+print("Executed")
 
